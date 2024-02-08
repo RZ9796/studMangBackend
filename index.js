6a296cce2054dbc7744ca9a5beb6e7905f056fb5
RZ9796 <rushizalte2001@gmail.com>
2024-02-08 18:18:26 +0530

    404 update

--- index.js
+++ index.js
@@ -12,12 +12,13 @@ app.use(express.static(path.join(__dirname, "dist", "index.html")));
 app.use(express.json());
 // routes
 app.use("/api/admin", require("./routes/adminRoute"));
-// app.use("*", (req, res) => {
-//   res.status(404).json({ message: "Resource not found " });
-// });
-// app.use((err, req, res, next) => {
-//   res.status(500).json({ message: err.message || "Something went wrong " });
-// });
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  // res.status(404).json({ message: "Resource not found " });
+});
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message || "Something went wrong " });
+});
 
 mongoose.connection.once("open", () => {
   console.log(process.env.PORT);
